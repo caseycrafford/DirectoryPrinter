b6d308be48bdbc69f19f7bdfba894c42e8c13d08
caseycrafford <kccrafford@gmail.com>
2022-03-23 15:12:11 +0200

    Initial and final commit

---
+++ src/main/java/za/co/workpool/CollectionsPractice/App.java
@@ -0,0 +1,47 @@
+package za.co.workpool.CollectionsPractice;
+
+import java.io.File;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	static int tabbingInt = 0;
+	
+	
+	public static void main(String[] args) {
+		File currentDir = new File("C:/Users/Developer"); 
+		displayDirectoryContents(currentDir);
+	}
+
+	
+	public static void displayDirectoryContents(File dir) {
+		try {
+			File[] files = dir.listFiles();
+			
+			for (File file : files) {
+				if (file.isDirectory()) {
+					tabbingInt++;
+					System.out.println(tabbing(tabbingInt) + "["+ file.getName()+ "]");
+					displayDirectoryContents(file);
+					tabbingInt--;
+				} else {
+					System.out.println(tabbing(tabbingInt+1) + "-"+ file.getName());
+				}
+			}
+			
+		} catch (NullPointerException e) {
+		}
+	}
+	
+
+	public static String tabbing(int tabs) {
+		String tabbing = "";
+		for(int i=0;i<tabs;i++) {
+			tabbing+="\t";
+		}
+		return tabbing;
+	}
+}
