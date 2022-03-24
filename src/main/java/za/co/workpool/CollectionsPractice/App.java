6d84ee7d61a9c2f6c584eabdb7b48c1bfdae0921
caseycrafford <kccrafford@gmail.com>
2022-03-24 12:40:21 +0200

    Created recursive method findFileInDirectory

--- src/main/java/za/co/workpool/CollectionsPractice/App.java
+++ src/main/java/za/co/workpool/CollectionsPractice/App.java
@@ -1,19 +1,24 @@
 package za.co.workpool.CollectionsPractice;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
-/**
- * Hello world!
- *
- */
+import org.apache.commons.io.FilenameUtils;
+
 public class App 
 {
 	static int tabbingInt = -1;
+	static Map<String, Integer> extensions= new HashMap<String, Integer>();
 	
-	
-	public static void main() {
+	public static void main(String[] args) {
 		File currentDir = new File("C:/Users/Developer"); 
-		displayDirectoryContents(currentDir);
+//		displayDirectoryContents(currentDir);
+//		System.out.println(extensions);
+		Set<String> searchRes = new HashSet<String>();
+		System.out.println(findFileInDirectory(currentDir, "Posit", searchRes));
 	}
 
 	
@@ -28,14 +33,33 @@ public class App
 					displayDirectoryContents(file);
 					tabbingInt--;
 				} else {
-					System.out.println(tabbing(tabbingInt+1) + "-"+ file.getName());
+					System.out.println(tabbing(tabbingInt+1) + "-"+ file.getCanonicalPath());
+					extensions.merge(FilenameUtils.getExtension(file.getName()), 1, Integer::sum);
 				}
 			}
 			
-		} catch (NullPointerException e) {
+		} catch (Exception e) {
 		}
 	}
 	
+	public static Set<String> findFileInDirectory(File dir,String fileName, Set<String> results) {
+		try {
+			File[] files = dir.listFiles();
+			
+			for (File file : files) {
+				if (file.isDirectory()) {
+					findFileInDirectory(file,fileName,results);
+				} else {
+					if(file.getName().contains(fileName)) {
+						results.add(file.getPath());
+					}
+				}
+			}
+			
+		} catch (Exception e) {
+		}
+		return results;
+	}
 
 	public static String tabbing(int tabs) {
 		String tabbing = "";
@@ -43,5 +67,11 @@ public class App
 			tabbing+="\t";
 		}
 		return tabbing;
+	}
+	
+	public static boolean testContentEquals() {
+		String search = "bar";
+		String toSearch = "barcelona";
+		return toSearch.contains(search);
 	}
 }
