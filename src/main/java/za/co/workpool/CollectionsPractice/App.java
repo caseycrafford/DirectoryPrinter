d683cfcd8dff94c47fd23c5d349aec403b20d69f
caseycrafford <kccrafford@gmail.com>
2022-03-24 16:59:24 +0200

    Added console line GUI to choose to search or view directory

--- src/main/java/za/co/workpool/CollectionsPractice/App.java
+++ src/main/java/za/co/workpool/CollectionsPractice/App.java
@@ -3,7 +3,10 @@ package za.co.workpool.CollectionsPractice;
 import java.io.File;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.InputMismatchException;
 import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 import java.util.Set;
 
 import org.apache.commons.io.FilenameUtils;
@@ -14,11 +17,7 @@ public class App
 	static Map<String, Integer> extensions= new HashMap<String, Integer>();
 	
 	public static void main(String[] args) {
-		File currentDir = new File("C:/Users/Developer"); 
-//		displayDirectoryContents(currentDir);
-//		System.out.println(extensions);
-		Set<String> searchRes = new HashSet<String>();
-		System.out.println(findFileInDirectory(currentDir, "Posit", searchRes));
+		menu();
 	}
 
 	
@@ -33,7 +32,7 @@ public class App
 					displayDirectoryContents(file);
 					tabbingInt--;
 				} else {
-					System.out.println(tabbing(tabbingInt+1) + "-"+ file.getCanonicalPath());
+					System.out.println(tabbing(tabbingInt+1) + "-"+ file.getName());
 					extensions.merge(FilenameUtils.getExtension(file.getName()), 1, Integer::sum);
 				}
 			}
@@ -73,5 +72,42 @@ public class App
 		String search = "bar";
 		String toSearch = "barcelona";
 		return toSearch.contains(search);
+	}
+	
+	public static void menu() {
+		Scanner sysIn = new Scanner(System.in);
+		try  {
+			String choice = "";
+			Set<String> results = new HashSet<String>();
+			
+			System.out.println("1. View a directory \n2. Find a file in a directory");
+			choice = sysIn.next();
+			
+			if (choice.equals("1")) {
+				System.out.println("Paste the directory to be viewed below:");
+				File currentDir = new File(sysIn.next());
+				displayDirectoryContents(currentDir);
+			} else if (choice.equals("2")) {
+				System.out.println("Paste the directory you would like to search below:");
+				File currentDir = new File(sysIn.next());
+				System.out.print("Enter search term:");
+				String searchString = sysIn.next();
+				findFileInDirectory(currentDir, searchString, results);
+				System.out.println("========================================================\n\t\t\tResults\n========================================================");
+				for(String result:results) {
+					System.out.println(result);
+				}
+				System.out.println("========================================================\n\t\t\tEnd\n========================================================");
+			} else 
+				throw new InputMismatchException(); 
+		} catch (InputMismatchException e) {
+			System.out.println("Only 1 or 2 is allowed.");
+			menu();
+		} catch (NoSuchElementException e) {
+			System.out.println("Only 1 or 2 is allowed.");
+			menu();
+		} finally {
+			sysIn.close();
+		}
 	}
 }
--- src/test/java/za/co/workpool/CollectionsPractice/AppTest.java
+++ src/test/java/za/co/workpool/CollectionsPractice/AppTest.java
@@ -1,38 +1,31 @@
 package za.co.workpool.CollectionsPractice;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertTrue;  
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import org.junit.jupiter.api.Test;
+ 
+public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	void appTest() {
+		boolean bool = false;
+		try (Scanner sysIn = new Scanner(System.in)) {
+			System.out.println("Enter int:");
+			int choice = sysIn.nextInt();
+
+			if (choice != 1 && choice != 2) {
+				System.out.println("must be 1 or 2");
+				throw new InputMismatchException();
+			} else
+				bool = true;
+
+		} catch (InputMismatchException e) {
+			System.out.println(e);
+		}
+		assertTrue(bool);
+	}
+
 }
