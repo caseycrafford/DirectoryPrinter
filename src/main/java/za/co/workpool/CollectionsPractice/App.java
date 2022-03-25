a754d27d0b94144e278cc6fe375f2b2deeba2490
caseycrafford <kccrafford@gmail.com>
2022-03-25 16:56:39 +0200

    Many changes Changed if to switch Made displayExtensionsInDirectory() Changed recursive menu() to have a while statement

--- src/main/java/za/co/workpool/CollectionsPractice/App.java
+++ src/main/java/za/co/workpool/CollectionsPractice/App.java
@@ -14,12 +14,15 @@ public class App
 {
 	static int tabbingInt = -1;
 	static Map<String, Integer> extensions= new HashMap<String, Integer>();
+	static Scanner sysIn;
+	static String resultStart="========================================================\\n\\t\\t\\tResults\\n========================================================";
+	static String resultEnd="========================================================\\n\\t\\t      End of Results\\n========================================================";
+	
 	
 	public static void main(String[] args) {
 		menu();
 	}
 
-	
 	public static void displayDirectoryContents(File dir) {
 		try {
 			File[] files = dir.listFiles();
@@ -35,11 +38,10 @@ public class App
 					extensions.merge(FilenameUtils.getExtension(file.getName()), 1, Integer::sum);
 				}
 			}
-			
 		} catch (Exception e) {
 		}
 	}
-	
+
 	public static Set<String> findFileInDirectory(File dir,String fileName, Set<String> results) {
 		try {
 			File[] files = dir.listFiles();
@@ -53,7 +55,24 @@ public class App
 					}
 				}
 			}
+		} catch (Exception e) {
+		}
+		return results;
+	}
+
+	public static Set<String> displayExtensionInDirectory(File dir,String extName, Set<String> results) {
+		try {
+			File[] files = dir.listFiles();
 			
+			for (File file : files) {
+				if (file.isDirectory()) {
+					displayExtensionInDirectory(file,extName,results);
+				} else {
+					if(FilenameUtils.getExtension(file.getName()).equalsIgnoreCase(extName)) {
+						results.add(file.getPath());
+					}
+				}
+			}
 		} catch (Exception e) {
 		}
 		return results;
@@ -68,44 +87,30 @@ public class App
 	}
 	
 	public static void menu() {
-		Scanner sysIn = new Scanner(System.in);
+		sysIn = new Scanner(System.in);
 		String choice;
-		Set<String> results = new HashSet<String>();
 		boolean repeatMenu = true;
 		
 		while (repeatMenu) {
-			
-			System.out.println("\n1. View a directory \n2. Find a file in a directory \n3. Exit");
+			System.out.println("\n1. View a directory \n2. Find a file in a directory \n3.Search by file extension \n4. Exit");
 			choice = sysIn.next();
 			try {
 				switch (choice) {
 				case "1":
-					System.out.println("Paste the directory to be viewed below:");
-					File displayDir = new File(sysIn.next());
-					System.out.println(
-							"========================================================\n\t\t\tResults\n========================================================");
-					displayDirectoryContents(displayDir);
-					System.out.println(
-							"========================================================\n\t\t      End of Results\n========================================================");
+					viewDirectoryMenu();
 					break;
 
 				case "2":
-					System.out.println("Paste the directory you would like to search below:");
-					File currentDir = new File(sysIn.next());
-					System.out.print("Enter search term:");
-					String searchString = sysIn.next();
-					findFileInDirectory(currentDir, searchString, results);
-					System.out.println(
-							"========================================================\n\t\t\tResults\n========================================================");
-					for (String result : results) {
-						System.out.println(result);
-					}
-					System.out.println(
-							"========================================================\n\t\t     End of Results\n========================================================");
+					findFileMenu();
 					break;
+					
 				case "3":
+					findByExtensionMenu();
+					break;
+					
+				case "4":
 					System.exit(0);
-
+					
 				default:
 					System.out.println("\nPlease enter 1, 2 or 3\n");
 					break;
@@ -115,5 +120,49 @@ public class App
 			} 
 		}
 		sysIn.close();
+	}
+	
+	public static void viewDirectoryMenu() {
+		System.out.println("Paste the directory to be viewed below:");
+		File displayDir = new File(sysIn.next());
+		System.out.println(resultStart);
+		displayDirectoryContents(displayDir);
+		System.out.println(resultEnd);
+	}
+	
+	public static void findFileMenu() {
+		Set<String> results = new HashSet<String>();
+		
+		System.out.println("Paste the directory you would like to search below:");
+		File currentDir = new File(sysIn.next());
+		
+		System.out.print("Enter search term:");
+		String searchString = sysIn.next();
+		
+		findFileInDirectory(currentDir, searchString, results);
+		System.out.println(resultStart);
+		for (String result : results) {
+			System.out.println(result);
+		}
+		System.out.println(resultEnd);
+	}
+	
+	public static void findByExtensionMenu() {
+		Set<String> results = new HashSet<String>();
+		
+		System.out.println("Paste the directory you would like to search below:");
+		File extDir = new File(sysIn.next());
+		System.out.print("Enter extension:");
+		String searchString1 = sysIn.next();
+		if(searchString1.startsWith(".")) {
+			searchString1=searchString1.substring(1);
+		}
+		results.clear();
+		displayExtensionInDirectory(extDir, searchString1, results);
+		System.out.println(resultStart);
+		for (String result : results) {
+			System.out.println(result);
+		}
+		System.out.println(resultEnd);
 	}
 }
